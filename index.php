e4ce62ef1d57844b5ce98ad0240ce8da39ca0d77
EC2 Default User <ec2-user@ip-172-31-19-98.ec2.internal>
2026-04-20 03:49:44 +0000

    Fixed timezone to IST

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php echo '<h1>Dynamic Site Running on EC2</h1>'; echo '<p>Current Time: ' . date('Y-m-d H:i:s') . '</p>'; ?>
+<?php date_default_timezone_set('Asia/Kolkata'); echo '<h1>Dynamic Site Running on EC2</h1>'; echo '<p>Current Indian Standard Time: ' . date('Y-m-d h:i:s A') . '</p>'; ?>
