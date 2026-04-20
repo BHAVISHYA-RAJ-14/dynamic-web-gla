2997f5bc11956b647282d5aafa443b5338a3d0a4
EC2 Default User <ec2-user@ip-172-31-19-98.ec2.internal>
2026-04-20 03:53:00 +0000

    Updated site heading

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php date_default_timezone_set('Asia/Kolkata'); echo '<h1>Dynamic Site Running on EC2</h1>'; echo '<p>Current Indian Standard Time: ' . date('Y-m-d h:i:s A') . '</p>'; ?>
+<?php date_default_timezone_set('Asia/Kolkata'); echo '<h1>BH dynamic web powered by github</h1>'; echo '<p>Current Indian Standard Time: ' . date('Y-m-d h:i:s A') . '</p>'; ?>
